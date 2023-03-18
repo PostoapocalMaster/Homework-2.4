ea498586ed56dd1ccbf64dda7799379ce3fbbd69
PostoapocalMaster <85695740+PostoapocalMaster@users.noreply.github.com>
2023-03-18 19:45:22 +0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <stdio.h>
+
+/*https://github.com/PostoapocalMaster/Homework-2.4*/
+
+main()
+{
+	int day;
+	printf("Day?");
+	scanf("%i,",&day);
+	
+	switch(day)
+	{
+		case 1:
+			{
+				printf("Monday");
+			}
+			break;
+		case 2:
+			{
+				printf("Tuesday");
+			}
+			break;
+		case 3:
+			{
+				printf("Wednesday");
+			}
+			break;
+		case 4:
+			{
+				printf("Thursday");
+			}
+			break;
+		case 5:
+			{
+				printf("Friday");
+			}
+			break;
+		
+		case 6:
+			{
+				printf("Saturday");
+			}
+			break;
+		case 7:
+			{
+				printf("Sunday");
+			}
+			break;
+		default:
+			{
+				printf("ERROR");
+			}
+ 	}
+}
